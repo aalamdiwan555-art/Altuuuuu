d6309e1095903541d0a52d595b4e58b9ca7b19a7
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-09-04 10:29:47 +0530

    Add account access files: smartautoclicker/src/main/java/com/buzbuz/smartautoclicker/auth/AuthStore.kt

---
+++ smartautoclicker/src/main/java/com/buzbuz/smartautoclicker/auth/AuthStore.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2026 Altuuuuu contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ */
+package com.buzbuz.smartautoclicker.auth
+
+import android.content.Context
+
+internal class AuthStore(context: Context) {
+
+    private val preferences = context.getSharedPreferences("altuuuuu_auth", Context.MODE_PRIVATE)
+
+    val accessToken: String?
+        get() = preferences.getString(KEY_ACCESS_TOKEN, null)
+
+    val refreshToken: String?
+        get() = preferences.getString(KEY_REFRESH_TOKEN, null)
+
+    fun saveSession(accessToken: String, refreshToken: String?) {
+        preferences.edit()
+            .putString(KEY_ACCESS_TOKEN, accessToken)
+            .apply {
+                if (refreshToken == null) remove(KEY_REFRESH_TOKEN)
+                else putString(KEY_REFRESH_TOKEN, refreshToken)
+            }
+            .apply()
+    }
+
+    fun clear() {
+        preferences.edit().clear().apply()
+    }
+
+    private companion object {
+        const val KEY_ACCESS_TOKEN = "access_token"
+        const val KEY_REFRESH_TOKEN = "refresh_token"
+    }
+}
